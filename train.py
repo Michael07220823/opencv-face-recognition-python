54e5121a8030cf40f25bd213ef863d96285d138d
Overcomer <michael31703@gmail.com>
2020-02-13 22:54:19 +0800

    Simplified code.

--- train.py
+++ train.py
@@ -1,47 +1,38 @@
-import cv2
 import os
+import cv2
 import numpy as np
-from Face import prepare_training_data, predict
+from Face import prepare_training_data, predict, classification_label, face_recognizer
 
 
-#there is no label 0 in our training data so subject name for index/label 0 is empty
-subjects = ["unknow", "Ramiz Raja", "Elvis Presley", "Michael"]
-
-print("Preparing data...")
+print("[FACE] Preparing data...")
 faces, labels = prepare_training_data("training-data")
+print("[FACE] Data prepared...")
 
-print("Data prepared")
-
-#print total faces and labels
-print("Total faces: ", len(faces))
-print("Total labels: ", len(labels))
+# Print total faces and labels
+print("[FACE] Total faces: ", len(faces))
+print("[FACE] Total labels: ", len(labels))
 
 
-#create our LBPH face recognizer 
-face_recognizer = cv2.face.LBPHFaceRecognizer_create()
-
-#train our face recognizer of our training faces
+# Train our face recognizer of our training faces
 face_recognizer.train(faces, np.array(labels))
 
 
-
-print("Predicting images...")
-
-#load test images
+# Load test images
 test_img1 = cv2.imread("test-data/test1.jpg")
 test_img2 = cv2.imread("test-data/test2.jpg")
 test_img3 = cv2.imread("test-data/test3.jpg")
 
-#perform a prediction
-predicted_img1 = predict(test_img1, face_recognizer, subjects)
-predicted_img2 = predict(test_img2, face_recognizer, subjects)
-predicted_img3 = predict(test_img3, face_recognizer, subjects)
-print("Prediction complete")
+# predict image.
+print("[FACE] Predicting images...")
+predicted_img1 = predict(test_img1)
+predicted_img2 = predict(test_img2)
+predicted_img3 = predict(test_img3)
+print("[FACE] Prediction complete")
 
-#display both images
-cv2.imshow(subjects[1], cv2.resize(predicted_img1, (400, 500)))
-cv2.imshow(subjects[2], cv2.resize(predicted_img2, (400, 500)))
-cv2.imshow(subjects[3], cv2.resize(predicted_img3, (400, 500)))
+# Display both images
+cv2.imshow(classification_label[1], cv2.resize(predicted_img1, (400, 500)))
+cv2.imshow(classification_label[2], cv2.resize(predicted_img2, (400, 500)))
+cv2.imshow(classification_label[3], cv2.resize(predicted_img3, (400, 500)))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
