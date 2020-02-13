3ac5922a1c20e293ce07d09b832614115cc8a9bd
Overcomer <michael31703@gmail.com>
2020-02-13 22:51:22 +0800

    Add predict program.

---
+++ predict.py
@@ -0,0 +1,15 @@
+import cv2
+from Face import predict, classification_label, face_recognizer
+
+# Load LBPH model.
+face_recognizer.read("model/model.yaml")
+
+# Load test image.
+read_image = cv2.imread("test-data/test5.jpg")
+
+# Predict image.
+predicted_img = predict(read_image)
+
+cv2.imshow(classification_label[3], cv2.resize(predicted_img, (400, 500)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
